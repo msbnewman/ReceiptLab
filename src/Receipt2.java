63c0602b3c7ef1a487c8feb380743c3d21383ee5
newman-coding <usmakerspace@nemwanschool.org>
2025-09-03 15:50:07 -0500

    Initial commit

---
+++ src/Receipt2.java
@@ -0,0 +1,22 @@
+/**
+ * Receipt2.java  
+ *
+ * @author – Your name
+ * @author – Class period
+ * @since - Today's date
+ */ 
+
+/* Modify the code from ReceiptDemo to include the following variables: 
+    - your high school’s name 
+    - the names of 4 items available at the student store
+    - the prices of each of the 4 items above
+    Be sure to use the most appropriate data types to store the values. 
+
+    Once the variables are declared and initialized, 
+    use string concatenation to modify the print statements from Receipt1.java 
+    to use the variables declared in this part. 
+*/
+
+public class Receipt2 {
+    
+}
---
+++ src/Receipt3.java
@@ -0,0 +1,54 @@
+/**
+ * Receipt3.java  
+ *
+ * @author – Your name
+ * @author – Class period
+ * @since - Today's Date
+ */ 
+
+/* 
+ * Add the following variables to your code:
+ * - orderNumber 
+ * - number of item1 ordered
+ * - number of item2 ordered
+ * - number of item3 ordered
+ * - number of item4 ordered
+ * - tax rate
+ * - subtotal (order total before tax is applied)
+ * - total tax
+ * - order total
+ * 
+ * To determine the number of each item ordered, your program should 
+ * generate a random number between 0 and 5, inclusive. 
+ * 
+ * To determine the orderNumber, generate a random integer
+ * between 1 inclusive and 100 exclusive. 
+*/
+
+public class Receipt3 {
+    
+}
+
+//Example Outpit:
+
+//*                  Welcome                     * 
+//*             Example HS Snack Bar             * 
+//*                                              * 
+//*     Drink ..................$ 1.5            * 
+//*     Candy ..................$ 1.25           * 
+//*     Hot Dog ................$ 2.75           * 
+//*     Hamburger ..............$ 3.5            *
+//*                                              * 
+//************************************************ 
+//*     Order Number    35                       * 
+//*                                              * 
+//*      QTY          ITEM             TOTAL     * 
+//************************************************
+//*       1           Drink            1.50      *
+//*       0           Candy            0.00      *
+//*       1           Hot Dog          2.75      *
+//*       1           Hamburger        3.50      *
+//************************************************
+//*       Subtotal                     7.75      *
+//*       Tax                          0.38      *
+//*       Total                        8.13      *
